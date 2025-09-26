594d97ea6054b4df122861cc2c9b0106a24f0af1
VIKASHINI SIVASELVAMOORTHI <167671299+VIKASHINI14092004@users.noreply.github.com>
2025-09-27 00:50:12 +0530

    Update 01.2_SUM_PROBLEM.java

--- 1.ARRAY/MEDIUM/01.2_SUM_PROBLEM.java
+++ 1.ARRAY/MEDIUM/01.2_SUM_PROBLEM.java
@@ -15,19 +15,59 @@ Output: [1,2]
 
 SOLUTION
 */
+//APPROACH 1
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       int arr[]=new int [nums.length];
-      for(int i=0;i<nums.length;i++)
-        {
-          arr[i]=nums[i];
-        }
-      Arrays.sort(arr[i]);
-      int start=0;
-      int end=nums.length-1;
-      while(start
-        return new int[]{};
+        int arr[][]=new int[nums.length][2];
+        for(int i=0;i<nums.length;i++)
+            {
+                arr[i][0]=nums[i];
+                arr[i][1]=i;
+            }
+        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+        int start=0;
+        int end=nums.length-1;
+        int sum=0;
+        while(start<end)
+            {
+                sum=arr[start][0]+arr[end][0];
+                if(sum==target)
+                {
+                    return new int[]{arr[start][1],arr[end][1]};
+                }
+                else if(sum>target)
+                {
+                    end-=1;
+                }
+                else
+                {
+                    start+=1;
+                }
+            }  
+        return new int[]{-1,-1};
     }
 }
+// TIME COMPLEXITY = O(n log n)
+// SPACE COMPLEXITY = O(N)
+
+//APPROACH 2
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+            {
+                int compare=target-nums[i];
+                if(map.containsKey(compare))
+                   {
+                    return new int[]{map.get(compare),i};
+                    }
+                map.put(nums[i],i);
+            }
+        return new int[]{-1,-1};
+        
+    }
+}
+// TIME COMPLEXITY = O(N)
+// SPACE COMPLEXITY = O(N)
 
 
